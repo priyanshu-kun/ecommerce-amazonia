93cc6b94bbe28d94a880200911239ada18d42b39
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2021-05-25 11:36:20 +0530

    add enviroment vriable logic in db connection file

--- server/DB/db.config.js
+++ server/DB/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/amazonia",
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/amazonia",
     {
         useNewUrlParser: true,
         useCreateIndex: true,
